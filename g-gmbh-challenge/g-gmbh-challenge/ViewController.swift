790d39fee40b017fecb27683cea2ff67be787c9c
Omar Samir <omarsamir@live.co.uk>
2019-02-12 21:42:25 +0200

    init commit

---
+++ g-gmbh-challenge/g-gmbh-challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  g-gmbh-challenge
+//
+//  Created by Omar Samir on 2/12/19.
+//  Copyright © 2019 Omar Samir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
